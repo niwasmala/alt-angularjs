ae0be838542a90847e6d24af0550c0120d6ec4e4
Nicky Irawan <niwasmala@gmail.com>
2015-07-16 10:36:43 +0700

    Version 2.1.1 - route fix

--- dist/alt/i18n.js
+++ dist/alt/i18n.js
@@ -1,1 +1,1 @@
-define([],function(){alt.language="id",alt.dictionary={id:{}},alt.modules.i18n=angular.module("alt-i18n",[]).factory("$i18n",["$log",function(a){return function(a){return alt.dictionary[alt.language]?alt.dictionary[alt.language][a]||a:a}}]),alt.module("alt-i18n",alt.modules.i18n)});
+alt.language="id",alt.dictionary=alt.extend({id:{}},alt.dictionary),alt.modules.i18n=angular.module("alt-i18n",[]).factory("$i18n",["$log",function(a){return function(a){return alt.dictionary[alt.language]?alt.dictionary[alt.language][a]||a:a}}]),alt.module("alt-i18n",alt.modules.i18n);
