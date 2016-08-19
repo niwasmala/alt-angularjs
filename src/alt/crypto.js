90b95c1adbaca49d4a3fdcad5d645e169765000c
Nicky Irawan <niwasmala@gmail.com>
2016-08-19 08:13:33 +0700

    5.2.0

--- src/alt/crypto.js
+++ src/alt/crypto.js
@@ -6,37 +6,39 @@ alt.loader.crypto = function(){
         .factory('$crypto', ['$log', '$timeout', '$rootScope', function($log, $timeout, $rootScope){
             return {
                 encrypt: function(data, key, iv){
-                    var mode = key.length == 16;
-                    var enc_key = CryptoJS.enc[mode ? 'Utf8' : 'Base64'].parse(key + '');
-                    var enc_iv = CryptoJS.enc.Base64.parse(iv + '');
+                    var enc_key = CryptoJS.enc.Utf8.parse(key + '');
+                    var enc_iv = CryptoJS.enc.Utf8.parse(iv + '');
+
                     var encrypted = CryptoJS.AES.encrypt(
                         data,
                         enc_key,
-                        mode ? { padding: CryptoJS.pad.Pkcs7, iv: enc_iv} : { mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7, iv: enc_iv});
+                        {
+                            mode: CryptoJS.mode.CBC,
+                            padding: CryptoJS.pad.Pkcs7,
+                            iv: enc_iv
+                        });
 
-                    return encrypted.toString();
+                    return encrypted.ciphertext.toString();
                 },
                 decrypt: function(data, key, iv){
                     var decrypted = '';
                     try{
-                        var mode = key.length == 16;
-                        var encrypted = (data + '');
-                        var enc_key = CryptoJS.enc[mode ? 'Utf8' : 'Base64'].parse(key + '');
-                        var enc_iv = CryptoJS.enc.Base64.parse(iv + '');
-                        var decrypt = CryptoJS.AES.decrypt(encrypted, enc_key, mode ? { iv: enc_iv, padding: CryptoJS.pad.Pkcs7 } : { iv: enc_iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+                        var encrypted = CryptoJS.enc.Hex.parse(data + '');
+                        var enc_key = CryptoJS.enc.Utf8.parse(key + '');
+                        var enc_iv = CryptoJS.enc.Utf8.parse(iv + '');
+                        var decrypt = CryptoJS.AES.decrypt({
+                                ciphertext: encrypted
+                            }, enc_key, {
+                                mode: CryptoJS.mode.CBC,
+                                padding: CryptoJS.pad.Pkcs7,
+                                iv: enc_iv
+                            });
 
-                        decrypted = mode ? CryptoJS.enc.Utf8.stringify(decrypt) : (decrypt ? decrypt.toString(CryptoJS.enc.Utf8) : "");
-                        if(mode) {
-                            var tmp = "";
-                            for (var i = 0; i < decrypted.length; i++) {
-                                if (btoa(decrypted[i]) != "AA==") tmp += decrypted[i];
-                            }
-                            decrypted = tmp;
-                        }
+                        decrypted = decrypt.toString(CryptoJS.enc.Utf8);
                     }catch(e){
-                        throw new Error("Tidak dapat melakukan parsing response dari server");
+                        throw e;
                     }
-                    
+
                     return decrypted;
                 }
             };
